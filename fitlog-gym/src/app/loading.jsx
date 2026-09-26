9cc4530c3c505c50e61450045644f404e43eae6b
MeenoolK <meenool369@gmail.com>
2026-09-26 13:10:55 +0600

    adding the loading function globally

---
+++ fitlog-gym/src/app/loading.jsx
@@ -0,0 +1,14 @@
+
+
+import React from 'react';
+
+const GlobalProcessing = () => {
+    return (
+        <div className=''>
+            The data is loading.....  Please hold your patience.
+            
+        </div>
+    );
+};
+
+export default GlobalProcessing;
